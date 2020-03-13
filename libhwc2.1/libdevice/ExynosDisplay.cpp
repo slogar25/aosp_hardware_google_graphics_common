b54e6ab2e7ea759d2e89183ab4e4551f75238150
HyunKyung Kim <hk310.kim@samsung.com>
2020-03-13 10:36:48 +0900

    libhwc2.1: Fix handling solid color layer having buffer
    HWC should handle the layer whose compositionType
    is HWC2_COMPOSITION_SOLID_COLOR as dim layer
    even if it has the buffer.
    
    Signed-off-by: HyunKyung Kim <hk310.kim@samsung.com>
    Change-Id: I2f5cf771fc850e7dee9d0fac269fdee577dadaa0

--- libhwc2.1/libdevice/ExynosDisplay.cpp
+++ libhwc2.1/libdevice/ExynosDisplay.cpp
@@ -1186,7 +1186,7 @@ int32_t ExynosDisplay::configureHandle(ExynosLayer &layer, int fence_fd, exynos_
     cfg.blending = blending;
     cfg.assignedMPP = otfMPP;
 
-    if (layer.isDimLayer() && handle == NULL) {
+    if (layer.isDimLayer()) {
         cfg.state = cfg.WIN_STATE_COLOR;
         hwc_color_t color = layer.mColor;
         cfg.color = (color.a << 24) | (color.r << 16) | (color.g << 8) | color.b;
