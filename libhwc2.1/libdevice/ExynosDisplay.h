f96198390e32d9fbf6ceb7f2aab5a0c5735b5570
Adrian Salido <salidoa@google.com>
2022-02-16 10:02:14 -0800

    libhwc2.1: implement boot time resolution API
    Implement boot time to get notified about resolution to be used at boot.
    
    Test: reboot device and check dumpsys display for boot mode
    Bug: 217399988
    Change-Id: I6034fa0dfee8a0d21bba316bb0dc11c300ad88fe

--- libhwc2.1/libdevice/ExynosDisplay.h
+++ libhwc2.1/libdevice/ExynosDisplay.h
@@ -953,7 +953,7 @@ class ExynosDisplay {
          *     HWC2_ERROR_BAD_CONFIG - when the configuration is invalid
          *     HWC2_ERROR_UNSUPPORTED - when the display does not support boot display config
          */
-        int32_t setBootDisplayConfig(int32_t config);
+        virtual int32_t setBootDisplayConfig(int32_t config);
 
         /**
          * clearBootDisplayConfig(...)
@@ -967,7 +967,7 @@ class ExynosDisplay {
          *     HWC2_ERROR_BAD_DISPLAY - when the display is invalid
          *     HWC2_ERROR_UNSUPPORTED - when the display does not support boot display config
          */
-        int32_t clearBootDisplayConfig();
+        virtual int32_t clearBootDisplayConfig();
 
         /**
          * getPreferredBootDisplayConfig(..., config*)
@@ -990,6 +990,8 @@ class ExynosDisplay {
          *     HWC2_ERROR_UNSUPPORTED - when the display does not support boot display config
          */
         int32_t getPreferredBootDisplayConfig(int32_t* outConfig);
+
+        virtual int32_t getPreferredDisplayConfigInternal(int32_t *outConfig);
 
         /* setAutoLowLatencyMode(displayToken, on)
          * Descriptor: HWC2_FUNCTION_SET_AUTO_LOW_LATENCY_MODE
