c8cf535708b2a5594741311ca5b451d6eecff958
joenchen <joenchen@google.com>
2022-11-01 05:14:47 +0000

    HWC boots in FHD resolution
    It can avoid resolution switching during bootanimation in the most cases
    when the default resolution is FHD.
    
    Test: boot to home screen
    Bug: 256745308
    Change-Id: Iec6b30c671666471eae21174224bf6094c77bf35

--- libhwc2.1/libdevice/ExynosDisplay.h
+++ libhwc2.1/libdevice/ExynosDisplay.h
@@ -1262,6 +1262,10 @@ class ExynosDisplay {
         // check if there are any dimmed layers
         bool isMixedComposition();
         bool isPriorFrameMixedCompostion() { return mPriorFrameMixedComposition; }
+        int lookupDisplayConfigs(const int32_t& width,
+                                 const int32_t& height,
+                                 const int32_t& fps,
+                                 int32_t* outConfig);
 
     private:
         bool skipStaticLayerChanged(ExynosCompositionInfo& compositionInfo);
