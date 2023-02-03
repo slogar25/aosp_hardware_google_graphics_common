dc92fe4cffd1048fcc7775f4dc407ecfc340b852
Hao Dong <spdonghao@google.com>
2023-02-03 03:43:48 +0000

    Revert "libhwc2.1: cleanup of BrightnessController code"
    This reverts commit accbc1f00bf88af09aa69fc22bacbe54d1f76587.
    
    Reason for revert: b/267569180
    
    Change-Id: If5d09be016fa49b0ecdf3a13f06e624c7a0b55c3
    (cherry picked from commit f87ab4e4b8b0f97e50ae34ada3a048ba08d5aeea)
    Merged-In: If5d09be016fa49b0ecdf3a13f06e624c7a0b55c3

--- libhwc2.1/libdevice/BrightnessController.h
+++ libhwc2.1/libdevice/BrightnessController.h
@@ -114,7 +114,7 @@ public:
         std::lock_guard<std::recursive_mutex> lock(mBrightnessMutex);
         return mLhbm.get();
     }
-    int checkSysfsStatus(const std::string& file, const std::vector<std::string>& expectedValue,
+    int checkSysfsStatus(const char *file, const std::vector<std::string>& expectedValue,
                          const nsecs_t timeoutNs);
     void resetLhbmState();
 
@@ -142,21 +142,6 @@ public:
     void setOutdoorVisibility(LbeState state);
 
     int updateCabcMode();
-
-    const std::string GetPanelSysfileByIndex(const char *file_pattern) {
-        String8 nodeName;
-        nodeName.appendFormat(file_pattern, mPanelIndex);
-        return nodeName.c_str();
-    }
-
-    const std::string GetPanelRefreshRateSysfile() {
-        String8 nodeName;
-        nodeName.appendFormat(kRefreshrateFileNode,
-                              mPanelIndex == 0           ? "primary"
-                                      : mPanelIndex == 1 ? "secondary"
-                                                         : "unknown");
-        return nodeName.c_str();
-    }
 
     struct BrightnessTable {
         float mBriStart;
