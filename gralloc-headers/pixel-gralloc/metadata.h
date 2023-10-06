7cb8ad0b54e03992aeef4daec330bc40ace8d0eb
Ankit Goyal <layog@google.com>
2023-10-05 21:26:43 -0700

    Add encode and decode utils
    Bug: 176048725
    Test: Boot to home
    Test: Custom logs in the graphics driver
    Change-Id: Ie440138c9d483158452e9715b9803586b68e2339

--- gralloc-headers/pixel-gralloc/metadata.h
+++ gralloc-headers/pixel-gralloc/metadata.h
@@ -51,39 +51,20 @@ enum class MetadataType : int64_t {
 
     // TODO: These metadata queries returns a pointer inside metadata for now. Need to change that
     // so we are returning proper data only.
+    // Returns: void*
     VIDEO_HDR = std::numeric_limits<int64_t>::max() - (1 << 16),
 
     // TODO(b/289448426#comment2): ROIINFO is probably not being used. Remove this after
     // confirmation.
+    // Returns: void*
     VIDEO_ROI,
 
     // This metadata just refers to the same fd contained in buffer handle and not a clone.
     // So the client should not attempt to close these fds.
+    // Returns: std::vector<int>
     PLANE_DMA_BUFS,
 };
 
 #undef MapMetadataType
-
-namespace util {
-
-template<typename T>
-static android::hardware::hidl_vec<uint8_t> encodeVector(const std::vector<T>& input) {
-    const size_t kVecSize = sizeof(T) * input.size();
-    android::hardware::hidl_vec<uint8_t> output(kVecSize);
-    std::memcpy(output.data(), input.data(), kVecSize);
-
-    return output;
-}
-
-template<typename T>
-std::vector<T> decodeVector (const android::hardware::hidl_vec<uint8_t>& input) {
-    const size_t kVecSize = input.size();
-    assert(kVecSize % sizeof(T) == 0);
-    std::vector<T> output(kVecSize / sizeof(T));
-    std::memcpy(output.data(), input.data(), kVecSize);
-
-    return output;
-}
-} // namespace util
 
 } // namespace pixel::graphics
