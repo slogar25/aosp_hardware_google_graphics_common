605c2dd8b5bd23d5308d24e98794a8c1c1b9631b
Devika Krishnadas <kdevika@google.com>
2024-01-03 21:43:14 +0000

    Add maybe_unused function attribute to template specializations
    Bug: 276384464
    Test: Build
    
    Change-Id: I59f882153a0c12f0a4252b6c107569502eb6fa01
    Signed-off-by: Devika Krishnadas <kdevika@google.com>

--- gralloc-headers/pixel-gralloc/mapper.h
+++ gralloc-headers/pixel-gralloc/mapper.h
@@ -46,34 +46,34 @@ static std::optional<typename ReturnType<T>::type> get(buffer_handle_t /*handle*
 }
 
 // TODO: Add support for stable-c mapper
-#define GET(metadata, return_type)                                                                \
-    template <>                                                                                   \
-    struct ReturnType<MetadataType::metadata> {                                                   \
-        using type = return_type;                                                                 \
-    };                                                                                            \
-                                                                                                  \
-    template <>                                                                                   \
-    std::optional<typename ReturnType<MetadataType::metadata>::type> get<MetadataType::metadata>( \
-            buffer_handle_t handle) {                                                             \
-        auto mapper = get_mapper();                                                               \
-        IMapper::MetadataType type = {                                                            \
-                .name = kPixelMetadataTypeName,                                                   \
-                .value = static_cast<int64_t>(MetadataType::metadata),                            \
-        };                                                                                        \
-                                                                                                  \
-        android::hardware::hidl_vec<uint8_t> vec;                                                 \
-        Error error;                                                                              \
-        auto ret = mapper->get(const_cast<native_handle_t*>(handle), type,                        \
-                               [&](const auto& tmpError,                                          \
-                                   const android::hardware::hidl_vec<uint8_t>& tmpVec) {          \
-                                   error = tmpError;                                              \
-                                   vec = tmpVec;                                                  \
-                               });                                                                \
-        if (!ret.isOk()) {                                                                        \
-            return {};                                                                            \
-        }                                                                                         \
-                                                                                                  \
-        return utils::decode<return_type>(vec);                                                   \
+#define GET(metadata, return_type)                                                       \
+    template <>                                                                          \
+    struct ReturnType<MetadataType::metadata> {                                          \
+        using type = return_type;                                                        \
+    };                                                                                   \
+                                                                                         \
+    template <>                                                                          \
+    [[maybe_unused]] std::optional<typename ReturnType<MetadataType::metadata>::type>    \
+    get<MetadataType::metadata>(buffer_handle_t handle) {                                \
+        auto mapper = get_mapper();                                                      \
+        IMapper::MetadataType type = {                                                   \
+                .name = kPixelMetadataTypeName,                                          \
+                .value = static_cast<int64_t>(MetadataType::metadata),                   \
+        };                                                                               \
+                                                                                         \
+        android::hardware::hidl_vec<uint8_t> vec;                                        \
+        Error error;                                                                     \
+        auto ret = mapper->get(const_cast<native_handle_t*>(handle), type,               \
+                               [&](const auto& tmpError,                                 \
+                                   const android::hardware::hidl_vec<uint8_t>& tmpVec) { \
+                                   error = tmpError;                                     \
+                                   vec = tmpVec;                                         \
+                               });                                                       \
+        if (!ret.isOk()) {                                                               \
+            return {};                                                                   \
+        }                                                                                \
+                                                                                         \
+        return utils::decode<return_type>(vec);                                          \
     }
 
 GET(PLANE_DMA_BUFS, std::vector<int>);
