3556b025e681ab95f664d2c1ce42dce6d52601a9
Chris Lu <luchris@google.com>
2024-01-03 09:28:06 +0000

    libhwc2.1: add debug type for operation rate
    Add the type to enable the log dynamically.
    
    Bug: 317854888
    Test: adb shell vndservice call Exynos.HWCService 105 i32 33554432
    Change-Id: I77a070462620623bc25f96dd5bd51b1e583dad5d

--- libhwc2.1/ExynosHWCDebug.h
+++ libhwc2.1/ExynosHWCDebug.h
@@ -47,6 +47,7 @@ enum {
     eDebugDisplayConfig           =   0x00400000,
     eDebugTDM                     =   0x00800000,
     eDebugLoadBalancing           =   0x01000000,
+    eDebugOperationRate           =   0x02000000,
 };
 
 class ExynosDisplay;
@@ -101,12 +102,18 @@ int32_t saveErrorLog(const android::String8& errString, const ExynosDisplay* dis
 
 #if defined(DISABLE_HWC_DEBUG)
 #define DISPLAY_LOGD(...)
+#define DISPLAY_STR_LOGD(...)
 #define MPP_LOGD(...)
 #else
 #define DISPLAY_LOGD(type, msg, ...) \
     {\
         if (hwcCheckDebugMessages(type)) \
             ALOGD("%s:: [%s] " msg, __func__, mDisplayName.c_str(), ##__VA_ARGS__); \
+    }
+#define DISPLAY_STR_LOGD(dispString, type, msg, ...) \
+    {\
+        if (hwcCheckDebugMessages(type)) \
+            ALOGD("%s:: [%s] " msg, __func__, dispString, ##__VA_ARGS__); \
     }
 #define MPP_LOGD(type, msg, ...) \
     {\
